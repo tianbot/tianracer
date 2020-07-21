cda53aad9d6b0f241174ea19292544c7f77e71ae
Tian Bo <tianb03@hotmail.com>
2020-07-21 18:04:15 +0800

    teb local planner

--- tianracer_navigation/script/cmd_vel_to_ackermann_drive.py
+++ tianracer_navigation/script/cmd_vel_to_ackermann_drive.py
@@ -4,7 +4,7 @@
 
 import rospy, math
 from geometry_msgs.msg import Twist
-from ackermann_msgs.msg import AckermannDriveStamped
+from ackermann_msgs.msg import AckermannDrive
 
 
 def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
@@ -18,17 +18,14 @@ def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
 def cmd_callback(data):
   global wheelbase
   global ackermann_cmd_topic
-  global frame_id
   global pub
   
   v = data.linear.x
   steering = convert_trans_rot_vel_to_steering_angle(v, data.angular.z, wheelbase)
   
-  msg = AckermannDriveStamped()
-  msg.header.stamp = rospy.Time.now()
-  msg.header.frame_id = frame_id
-  msg.drive.steering_angle = steering
-  msg.drive.speed = v
+  msg = AckermannDrive()
+  msg.steering_angle = steering
+  msg.speed = v
   
   pub.publish(msg)
   
@@ -44,12 +41,11 @@ if __name__ == '__main__':
     twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel') 
     ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', '/ackermann_cmd')
     wheelbase = rospy.get_param('~wheelbase', 1.0)
-    frame_id = rospy.get_param('~frame_id', 'odom')
     
     rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
-    pub = rospy.Publisher(ackermann_cmd_topic, AckermannDriveStamped, queue_size=1)
+    pub = rospy.Publisher(ackermann_cmd_topic, AckermannDrive, queue_size=1)
     
-    rospy.loginfo("Node 'cmd_vel_to_ackermann_drive' started.\nListening to %s, publishing to %s. Frame id: %s, wheelbase: %f", "/cmd_vel", ackermann_cmd_topic, frame_id, wheelbase)
+    rospy.loginfo("Node 'cmd_vel_to_ackermann_drive' started.\nListening to %s, publishing to %s. wheelbase: %f", "/cmd_vel", ackermann_cmd_topic, wheelbase)
     
     rospy.spin()
     
