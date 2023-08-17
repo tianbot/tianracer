f105d219e2d5db5095b15cbb7434a60c2fe9e713
tianb03 <tianbo@tianbot.com>
2023-08-17 19:07:27 +0800

    fix: add ignored maps. improve servo commands

--- tianracer_gazebo/scripts/servo_commands.py
+++ tianracer_gazebo/scripts/servo_commands.py
@@ -28,16 +28,35 @@ def set_throttle_steer(data):
     # calculate the right and left steering angle. 
     wheelbase = 0.265
     track_width = 0.13 # it is the distance between the two front hinges.
+    wheel_width = 0.18 # 0.18, 
+    hinge_length = 0.025
     tan_steer = math.tan(steering_angle)
     left_steer = math.atan2(wheelbase*tan_steer, wheelbase-track_width*tan_steer/2)
     right_steer = math.atan2(wheelbase*tan_steer, wheelbase+track_width*tan_steer/2)
     
+    # # calculate the linear velocity for each wheel
+    # # seems no obvious difference
+    
+    # angular_velocity = throttle*tan_steer/wheelbase
+    # if abs(steering_angle) > 0.0001:    
+    #     # rear using wheel width
+    #     v_lr = throttle - angular_velocity*wheel_width/2
+    #     v_rr = throttle + angular_velocity*wheel_width/2
+    #     # front
+    #     v_lf = (wheelbase/math.sin(left_steer) - hinge_length) * angular_velocity
+    #     v_rf = (wheelbase/math.sin(right_steer) + hinge_length) * angular_velocity
+    # else:
+    #     v_lr = v_rr = v_lf = v_rf =  throttle
+    
+    # print(throttle, v_lf,v_lr,v_rf,v_rr)
+    
+    v_lr = v_rr = v_lf = v_rf =  throttle
     # The wheel speed difference can be neglected.
     # the wheel speed is the same as the throttle.
-    pub_vel_left_rear_wheel.publish(throttle)
-    pub_vel_right_rear_wheel.publish(throttle)
-    pub_vel_left_front_wheel.publish(throttle)
-    pub_vel_right_front_wheel.publish(throttle)
+    pub_vel_left_rear_wheel.publish(v_lr)
+    pub_vel_right_rear_wheel.publish(v_rr)
+    pub_vel_left_front_wheel.publish(v_lf)
+    pub_vel_right_front_wheel.publish(v_rf)
     pub_pos_left_steering_hinge.publish(left_steer)
     pub_pos_right_steering_hinge.publish(right_steer)
 
