4bc95dd83fb2e76598472e67f0dd96eac9c4bf7d
lr-2002 <2629651228@qq.com>
2023-10-23 17:23:08 +0800

    feat: judge_system to judge position

--- tianracer_gazebo/scripts/judge_system.py
+++ tianracer_gazebo/scripts/judge_system.py
@@ -6,14 +6,27 @@
 import tkinter as tk
 import rospy 
 from std_msgs.msg import String
-score = 60
+import os
 from datetime import datetime
-
-
+import time
+from utils.position_check import *
+import subprocess  
 import random
 import threading
 random.seed(1)
 
+"""
+todo 
+    1. stop all thread when finish
+    2. stop when no feedback 
+    3. the 10 point will stll get score output
+
+
+"""
+
+
+
+map_list = [5, 5, 5, 5, 5, 5, 10, 10, 15]
 """
 todo:
     update the code backend
@@ -25,8 +38,11 @@ class Judger:
         self.root_window = tk.Tk()
 
         self.scores = 0
+        self.time_back = time.time()
         self.times = 0
         self.infos = 0
+        self.pass_point = None
+        self.start_flag= False
 
         self.root_window.geometry('800x600')
         self.root_window.title("Tianbot 官方监控系统")
@@ -43,36 +59,80 @@ class Judger:
         self.score_display = tk.Label(self.root_window, relief='sunken', text='6', anchor='w',  fg="black",font=('Times', 20, 'bold italic'), width=30, height=5,padx=3, pady=3,borderwidth=3)
         self.score_display.place(x=240, y =300, width=160, height=100)
 
-        self.start_button = tk.Button(self.root_window, text="启动",command=start)
+        self.start_button = tk.Button(self.root_window, text="启动",command=self.start_judge)
         self.start_button.place(x=80, y= 500, width=300,height=60)
-
 
         self.info_item = tk.Text(self.root_window, width=40, height=400)
         self.info_item.pack(side="right")
 
-        self.update_info_item("正在启动...")
-        self.update_info_item("成功启动程序，开始测试")
-        self.update_info_item("通过A点+3分")
+        # self.update_info_item("成功启动程序，开始测试")
+        # self.update_info_item("通过A点+3分")
         # update_info(self.info_item, "hello\n")   
 
+    def cal_time_score(self):
+        self.scores += round(35 * 30  / max(self.times, 30),3)
+        return self.scores
+
+    def run_command(self):
+        os.system("rosrun tianracer_gazebo f1tenth_racer.py")
+
+    def start_judge(self):
+        if self.start_flag == True:
+            return False
+        self.command_thread = threading.Thread(target=self.run_command)
+        self.command_thread.start()
+        self.update_info_item('目标代码已启动')
+        self.time_back = time.time()
+
+        self.start_flag =True
     def start(self):
+
         self.update_info()
         self.thread = threading.Thread(target=rospy.spin)
         self.thread.start()
+        self.update_info_item("测评系统已加载完成")
         self.root_window.mainloop()
     
     def update_info(self):
-        info = 132.123
-        self.score_display.configure(text=str(int(info)))
-        self.time_count.configure(text="10:02:32")
+        # info = 132.123
+        self.score_display.configure(text=self.scores)
+        now = datetime.now()
+        if self.start_flag:
+            self.times = round(time.time() - self.time_back, 3)
+        else:
+            self.times = 0.000
+        self.time_count.configure(text=self.times)
         self.root_window.after(100, self.update_info)
 
     def update_info_item(self, info):
+        if info is None:
+            return False
+        score = None
+        if isinstance(info, tuple):
+            point = info[1]
+            info = info[0]
+            if point >= 9:
+                return True
+            global map_list
+            score = map_list[point]
+            self.scores += score
+            self.pass_point = point
         now = datetime.now()
 
         current_time = '[' + now.strftime("%H:%M:%S")+']  '
-        self.info_item.insert(tk.INSERT, current_time+info+'\n')
+        if not score:
+            self.info_item.insert(tk.INSERT, current_time+info+'\n')
+        else:   
+            self.info_item.insert(tk.INSERT, current_time+info +' +' +str(score) +'分\n')
+
+        if self.pass_point is not None and self.pass_point == 8:
+            self.start_flag = False
+            self.cal_time_score()
+            self.info_item.insert(tk.INSERT, current_time+info +' +' +str(score) +'分\n')
+
         return True
+
+
 
 def start():
     return True
@@ -82,17 +142,18 @@ def start():
 #     return True
 
 
-def update_callback(info):
-    print("recv info", info.data)
-    judger.update_info_item(info.data)
+def update_callback(data):
+    res = analysis(data)
+    judger.update_info_item(res)
     
 
 if __name__=="__main__":
     judger = Judger()
 
     rospy.init_node("judger")
-    position_sub = rospy.Subscriber("/position", String, callback=update_callback)
+    # position_sub = rospy.Subscriber("/position", String, callback=update_callback)
     # rospy.spin()
+    position_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, callback=update_callback)
     judger.start()
 
 
