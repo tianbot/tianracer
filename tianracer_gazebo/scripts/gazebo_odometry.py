d8d4a7fa37c59647306507b8dd06085e2256157b
tianb03 <tianbo@tianbot.com>
2024-05-25 14:45:49 +0800

    feat: gazebo_odometry now calc from jointstates

--- tianracer_gazebo/scripts/gazebo_odometry.py
+++ tianracer_gazebo/scripts/gazebo_odometry.py
@@ -1,113 +1,81 @@
 #!/usr/bin/env python
 
-'''
-This script makes Gazebo less fail by translating gazebo status messages to odometry data.
-Since Gazebo also publishes data faster than normal odom data, this script caps the update to 20hz.
-Winter Guerra
-'''
-
 import rospy
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose, Twist, Transform, TransformStamped, Vector3Stamped
-from gazebo_msgs.msg import LinkStates
-from std_msgs.msg import Header
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import Pose, Twist, Transform, TransformStamped
 import numpy as np
 import math
-import tf
 import tf2_ros
-import tf2_geometry_msgs
 
 robot_name = rospy.get_param('robot_name', default="tianracer")
+wheel_radius = 0.032  # Wheel radius in meters
+wheel_base = 0.265     # Distance between left and right wheels
+
 class OdometryNode:
-    # Set publishers
     pub_odom = rospy.Publisher('odom', Odometry, queue_size=1)
 
     def __init__(self):
-        # init internals
-        self.last_received_pose = Pose()
-        self.last_received_pose.orientation.w = 1
-        self.last_received_twist = Twist()
-        self.last_recieved_stamp = None
-
-        # Set the update rate
-        rospy.Timer(rospy.Duration(.05), self.timer_callback) # 20hz
-
+        self.pose = Pose()
+        self.pose.orientation.w = 1  # initial quaternion needs to be normalized
+        self.velocity = 0  # Initialize linear velocity
+        self.angular_velocity = 0  # Initialize angular velocity
+        self.last_time = rospy.Time.now()
+        self.last_left_wheel_position = 0
+        self.last_right_wheel_position = 0
         self.tf_pub = tf2_ros.TransformBroadcaster()
+        rospy.Subscriber('/tianracer/joint_states', JointState, self.joint_state_callback)
+        rospy.Timer(rospy.Duration(0.05), self.timer_callback)  # 20 Hz
 
-        # Set subscribers
-        rospy.Subscriber('/gazebo/link_states', LinkStates, self.sub_robot_pose_update)
-
-    def sub_robot_pose_update(self, msg):
-        # Find the index of the racecar
-        try:
-            arrayIndex = msg.name.index(f'{robot_name}::{robot_name}/base_footprint')
-        except ValueError as e:
-            # Wait for Gazebo to startup
-            pass
-        else:
-            # Extract our current position information
-            self.last_received_pose = msg.pose[arrayIndex]
-            self.last_received_twist = msg.twist[arrayIndex]
-            
-            # get a Vector3Stamped from the twist_in_gazebo
-            twist_linear = Vector3Stamped()
-            twist_linear.vector = self.last_received_twist.linear
-            
-            # get a TransformStamped from the last_received_pose
-            twist_transform = TransformStamped()
-            
-            # last received pose is base_footprint in gazebo. twist is in gazebo frame.
-            # we want to transform the twist into the base_footprint frame. requires a inverse
-            q=tf.transformations.quaternion_inverse([self.last_received_pose.orientation.x, self.last_received_pose.orientation.y, self.last_received_pose.orientation.z, self.last_received_pose.orientation.w])
-            
-            twist_transform.transform.rotation.x = q[0]
-            twist_transform.transform.rotation.y = q[1]
-            twist_transform.transform.rotation.z = q[2]
-            twist_transform.transform.rotation.w = q[3]
-                
-            # transform the Vector3Stamped into the child frame, base_link
-            twist_linear = tf2_geometry_msgs.do_transform_vector3(twist_linear, twist_transform)
-            
-            ######### the result is not proper ###########
-            #print(twist_linear)
-
-            self.last_received_twist.linear = twist_linear.vector
+    def joint_state_callback(self, msg):
+        current_time = rospy.Time.now()
+        dt = (current_time - self.last_time).to_sec()
+        rospy.loginfo("dt: %f", dt)
         
-        self.last_recieved_stamp = rospy.Time.now()
-          
-            
-    def timer_callback(self, event):
-        if self.last_recieved_stamp is None:
-            return
+        # The state of each joint (revolute or prismatic) is defined by:
+        #  * the position of the joint (rad or m),
+        #  * the velocity of the joint (rad/s or m/s) and 
+        #  * the effort that is applied in the joint (Nm or N).
 
+        left_wheel_idx = msg.name.index('left_rear_wheel_joint')
+        right_wheel_idx = msg.name.index('right_rear_wheel_joint')
+        
+        left_wheel_position = msg.position[left_wheel_idx]
+        right_wheel_position = msg.position[right_wheel_idx]
+
+        # Calculate the traveled distance since last update
+        delta_left_wheel = (left_wheel_position - self.last_left_wheel_position) * wheel_radius
+        delta_right_wheel = (right_wheel_position - self.last_right_wheel_position) * wheel_radius
+
+        # Calculate average forward velocity and angular velocity
+        # self.velocity = (delta_left_wheel + delta_right_wheel) / 2 / dt
+        # self.angular_velocity = (delta_right_wheel - delta_left_wheel) / wheel_base / dt
+        self.velocity = (msg.velocity[left_wheel_idx] * wheel_radius + msg.velocity[right_wheel_idx] * wheel_radius) / 2
+        self.angular_velocity = (msg.velocity[right_wheel_idx] * wheel_radius - msg.velocity[left_wheel_idx] * wheel_radius) / wheel_base
+        
+        # Update the pose
+        self.pose.position.x += self.velocity * math.cos(self.pose.orientation.z) * dt
+        self.pose.position.y += self.velocity * math.sin(self.pose.orientation.z) * dt
+        self.pose.orientation.z += self.angular_velocity * dt
+
+        # Update last positions and time
+        self.last_left_wheel_position = left_wheel_position
+        self.last_right_wheel_position = right_wheel_position
+        self.last_time = current_time
+
+    def timer_callback(self, event):
         odom = Odometry()
-        odom.header.stamp = self.last_recieved_stamp
+        odom.header.stamp = self.last_time
         odom.header.frame_id = f'{robot_name}/odom'
         odom.child_frame_id = f'{robot_name}/base_footprint'
-        odom.pose.pose = self.last_received_pose
-        odom.twist.twist = self.last_received_twist
-        odom.pose.covariance =[1e-3, 0, 0, 0, 0, 0,
-						0, 1e-3, 0, 0, 0, 0,
-						0, 0, 1e6, 0, 0, 0,
-						0, 0, 0, 1e6, 0, 0,
-						0, 0, 0, 0, 1e6, 0,
-						0, 0, 0, 0, 0, 1e3]
-
-        odom.twist.covariance = [1e-9, 0, 0, 0, 0, 0, 
-                          0, 1e-3, 1e-9, 0, 0, 0,
-                          0, 0, 1e6, 0, 0, 0,
-                          0, 0, 0, 1e6, 0, 0,
-                          0, 0, 0, 0, 1e6, 0,
-                          0, 0, 0, 0, 0, 1e-9]
-
+        odom.pose.pose = self.pose
+        odom.twist.twist.linear.x = self.velocity
+        odom.twist.twist.angular.z = self.angular_velocity
 
         self.pub_odom.publish(odom)
 
         tf = TransformStamped(
-            header=Header(
-                frame_id=odom.header.frame_id,
-                stamp=odom.header.stamp
-            ),
+            header=odom.header,
             child_frame_id=odom.child_frame_id,
             transform=Transform(
                 translation=odom.pose.pose.position,
@@ -116,7 +84,6 @@ class OdometryNode:
         )
         self.tf_pub.sendTransform(tf)
 
-# Start the node
 if __name__ == '__main__':
     rospy.init_node("gazebo_odometry_node")
     node = OdometryNode()
