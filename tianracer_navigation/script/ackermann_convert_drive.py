bb9a3b6a28cdd5222d40cf57e44e140065eae69f
sujit-168 <su2054552689@gmail.com>
2026-01-07 11:27:35 +0800

    feat: Increase the speed update frequency to 50Hz for Formula Mini

--- tianracer_navigation/script/ackermann_convert_drive.py
+++ tianracer_navigation/script/ackermann_convert_drive.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python3
-# Author: tianbot
-# Description:  Convert ackermann_msgs/AckermannDriveStamped to ackermann_msgs/AckermannDrive
-
+# Author: tianbot (Modified for 50Hz upsampling)
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
-from geometry_msgs.msg import Twist
 
 class AckermannConverter:
     def __init__(self):
-        rospy.init_node('ackermann_stamped_converter', anonymous=True) 
+        rospy.init_node('ackermann_stamped_converter', anonymous=True)
+        
+        # 参数获取
         ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', 'ackermann_cmd')
         ackermann_stamped_cmd_topic = rospy.get_param('~ackermann_stamped_cmd_topic', 'ackermann_cmd_stamped')
+        
+        # 初始化为空，用于判断是否收到了第一条指令
+        self.latest_drive_msg = None
+        
+        # 发布者
         self.drive_pub = rospy.Publisher(ackermann_cmd_topic, AckermannDrive, queue_size=1)
+        
+        # 订阅者
         rospy.Subscriber(ackermann_stamped_cmd_topic, AckermannDriveStamped, self.convert_callback, queue_size=1)
+        
+        # --- 设定为 50Hz --- ，与下位机超时频率一致
+        # 频率 f = 50, 周期 T = 1/50 = 0.02s
+        self.timer = rospy.Timer(rospy.Duration(1.0/50.0), self.timer_callback)
 
     def convert_callback(self, msg):
-        """callback func"""
+        """将收到的 Stamped 消息解包并存入缓存"""
+        if self.latest_drive_msg is None:
+            self.latest_drive_msg = AckermannDrive()
+            
+        self.latest_drive_msg.speed = msg.drive.speed
+        self.latest_drive_msg.steering_angle = msg.drive.steering_angle
+        self.latest_drive_msg.acceleration = msg.drive.acceleration
+        self.latest_drive_msg.steering_angle_velocity = msg.drive.steering_angle_velocity
+        self.latest_drive_msg.jerk = msg.drive.jerk
 
-        drive_msg = AckermannDrive()
-        
-        # copy the message value
-        drive_msg.speed = msg.drive.speed
-        drive_msg.steering_angle = msg.drive.steering_angle
-        drive_msg.acceleration = msg.drive.acceleration
-        drive_msg.jerk = msg.drive.jerk
-        
-        # post the converted message
-        self.drive_pub.publish(drive_msg)
+    def timer_callback(self, event):
+        """以 50Hz 频率持续发布最新缓存的数据"""
+        if self.latest_drive_msg is not None:
+            self.drive_pub.publish(self.latest_drive_msg)
 
 if __name__ == '__main__':
     try:
--- tianracer_navigation/script/cmd_vel_to_ackermann_drive.py
+++ tianracer_navigation/script/cmd_vel_to_ackermann_drive.py
@@ -1,55 +1,77 @@
 #!/usr/bin/env python
-
-# Author: christoph.roesmann@tu-dortmund.de
+# -*- coding: utf-8 -*-
 
 import rospy, math
 from geometry_msgs.msg import Twist
 from ackermann_msgs.msg import AckermannDrive
 
+# 全局变量
+current_v = 0.0
+current_steering = 0.0
+has_received_data = False
 
 def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
-  if omega == 0 or v == 0:
-    return 0
-
-  radius = v / omega
-  return math.atan(wheelbase / radius)
-
+  # 1. 如果没有角速度输入，转角必为 0
+  if omega == 0:
+    return 0.0
+    
+  # 2. 核心数学转换：增加速度为0时的保护
+  if v == 0:
+    v_eff = 0.1 # 有角速度但没线速度时，给定一个有效线速度计算转角
+  else:
+    v_eff = v
+    
+  # 3. 计算基础转角并乘以放大因子 gain
+  steering = math.atan(wheelbase * omega / v_eff) # 保持弧度输出
+  return steering
 
 def cmd_callback(data):
-  global wheelbase
-  global ackermann_cmd_topic
-  global pub
+  global wheelbase, current_v, current_steering, has_received_data
   
+  has_received_data = True
   v = data.linear.x
-  steering = convert_trans_rot_vel_to_steering_angle(v, data.angular.z, wheelbase)
+  omega = data.angular.z
   
-  msg = AckermannDrive()
-  msg.steering_angle = steering
+  # 计算转角
+  current_steering = convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase)
+  
+  # 速度阈值钳位逻辑
   threshold = 0.5
-  if 0 < v and v < threshold:
+  if 0 < v < threshold:
     v = threshold
-  elif -threshold < v and v < 0:
+  elif -threshold < v < 0:
     v = -threshold
-  msg.speed = v
-  rospy.loginfo("Clamped v: %.3f", v)
-  pub.publish(msg)
+  current_v = v
+
+def timer_callback(event):
+  """以固定 50Hz 频率发布"""
+  global pub, current_v, current_steering, has_received_data
+  
+  if has_received_data:
+    msg = AckermannDrive()
+    msg.speed = current_v
+    msg.steering_angle = current_steering
+    pub.publish(msg)
 
 if __name__ == '__main__': 
   try:
-    
     rospy.init_node('cmd_vel_to_ackermann_drive')
         
-    twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel') 
-    ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', '/ackermann_cmd')
+    # 参数获取
+    twist_cmd_topic = rospy.get_param('~twist_cmd_topic', 'cmd_vel') 
+    ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', 'ackermann_cmd')
     wheelbase = rospy.get_param('~wheelbase', 1.0)
     
-    rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
-    pub = rospy.Publisher(ackermann_cmd_topic, AckermannDrive, queue_size=1)
     
-    rospy.loginfo("Node 'cmd_vel_to_ackermann_drive' started.\nListening to %s, publishing to %s. wheelbase: %f", "/cmd_vel", ackermann_cmd_topic, wheelbase)
+    pub = rospy.Publisher(ackermann_cmd_topic, AckermannDrive, queue_size=1)
+    rospy.Subscriber(twist_cmd_topic, Twist, cmd_callback, queue_size=1)
+
+    # --- 50Hz 定时器 ---
+    rospy.Timer(rospy.Duration(1.0/50.0), timer_callback)
+    
+    rospy.loginfo("Node started at 50Hz. Wheelbase: %f", wheelbase)
     
     rospy.spin()
     
   except rospy.ROSInterruptException:
     pass
-
