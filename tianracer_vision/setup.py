2f8e909e4ade07e3efa62d0ee71b93627f0ead71
sujit-168 <su2054552689@gmail.com>
2026-02-03 17:10:06 +0800

    bug: fixed the colcon build error for tianracer_vision

--- tianracer_vision/setup.py
+++ tianracer_vision/setup.py
@@ -1,23 +1,27 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'tianracer_vision'
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=[package_name],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        # ('share/ament_index/resource_index/packages',
-        #     ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='TianRacer Team',
-    maintainer_email='you@example.com',
-    description='Vision nodes for the TianRacer platform',
-    license='MIT',
-    extras_require={ 'test': ['pytest', 'flake8'], },
+    maintainer='tianbot',
+    maintainer_email='tianbot@tianbot.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'line_follower = tianracer_vision.line_follower:main'
